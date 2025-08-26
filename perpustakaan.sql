13070ad24249ac17b595583a2d033dc2c720727e
VTanujaya <vtanujaya6@gmail.com>
2025-08-26 18:44:36 +0700

    Update Sql

--- perpustakaan.sql
+++ perpustakaan.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Aug 22, 2025 at 12:28 PM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.0.28
+-- Host: 127.0.0.1
+-- Generation Time: Aug 26, 2025 at 01:43 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 7.4.29
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,6 +24,59 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tabel_anggota`
+--
+
+CREATE TABLE `tabel_anggota` (
+  `id_anggota` int(11) NOT NULL,
+  `nama_anggota` varchar(64) NOT NULL,
+  `tanggal_lahir` date NOT NULL,
+  `jenis_kelamin` varchar(16) NOT NULL,
+  `NIS` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tabel_buku`
+--
+
+CREATE TABLE `tabel_buku` (
+  `id_buku` int(11) NOT NULL,
+  `judul_buku` varchar(64) NOT NULL,
+  `penulis_buku` varchar(64) NOT NULL,
+  `penerbit_buku` varchar(64) NOT NULL,
+  `tahun_terbit` int(11) NOT NULL,
+  `nisn` int(11) NOT NULL,
+  `stok_buku` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tabel_buku`
+--
+
+INSERT INTO `tabel_buku` (`id_buku`, `judul_buku`, `penulis_buku`, `penerbit_buku`, `tahun_terbit`, `nisn`, `stok_buku`) VALUES
+(1, 'test1', 'test2', 'test3', 2020, 32274550, 3),
+(2, 'test2', 'test3', 'test4', 3, 322745, 6);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tabel_transaksi`
+--
+
+CREATE TABLE `tabel_transaksi` (
+  `id_transaksi` int(11) NOT NULL,
+  `tanggal_peminjaman` date NOT NULL,
+  `id_buku` int(11) NOT NULL,
+  `id_anggota` int(11) NOT NULL,
+  `status_transaksi` varchar(16) NOT NULL,
+  `tanggal_pengembalian` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tabel_user`
 --
 
@@ -31,7 +84,7 @@ CREATE TABLE `tabel_user` (
   `id_user` int(10) NOT NULL,
   `nama_user` varchar(30) NOT NULL,
   `password_user` varchar(64) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `tabel_user`
@@ -45,6 +98,24 @@ INSERT INTO `tabel_user` (`id_user`, `nama_user`, `password_user`) VALUES
 --
 
 --
+-- Indexes for table `tabel_anggota`
+--
+ALTER TABLE `tabel_anggota`
+  ADD PRIMARY KEY (`id_anggota`);
+
+--
+-- Indexes for table `tabel_buku`
+--
+ALTER TABLE `tabel_buku`
+  ADD PRIMARY KEY (`id_buku`);
+
+--
+-- Indexes for table `tabel_transaksi`
+--
+ALTER TABLE `tabel_transaksi`
+  ADD PRIMARY KEY (`id_transaksi`);
+
+--
 -- Indexes for table `tabel_user`
 --
 ALTER TABLE `tabel_user`
@@ -53,6 +124,24 @@ ALTER TABLE `tabel_user`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `tabel_anggota`
+--
+ALTER TABLE `tabel_anggota`
+  MODIFY `id_anggota` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tabel_buku`
+--
+ALTER TABLE `tabel_buku`
+  MODIFY `id_buku` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `tabel_transaksi`
+--
+ALTER TABLE `tabel_transaksi`
+  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `tabel_user`
